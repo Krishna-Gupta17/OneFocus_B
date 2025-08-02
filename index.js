b620759da6e4b47dab51770bc68afe6aac96462d
Krishna-Gupta17 <2024021228@mmmut.ac.in>
2025-08-02 13:08:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'one-focus-six.vercel.app', // or your frontend URL
+  credentials: true
+}));
+
 app.use(express.json());
 // MongoDB connection
 connectmongo(process.env.MONGODB_URI);
@@ -259,4 +263,5 @@ app.post('/api/users/:uid/videos', async (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+
 });
